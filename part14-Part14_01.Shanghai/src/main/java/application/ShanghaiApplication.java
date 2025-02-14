acef0499ed2c58eb35188df514e0eaa2b45bc1f4
Ahmad Hamzah <hamzaha@mcmaster.ca>
2025-02-14 16:10:32 -0500

    part 14

--- part14-Part14_01.Shanghai/src/main/java/application/ShanghaiApplication.java
+++ part14-Part14_01.Shanghai/src/main/java/application/ShanghaiApplication.java
@@ -1,9 +1,52 @@
 package application;
 
-public class ShanghaiApplication {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+
+
+
+public class ShanghaiApplication extends Application{
+    @Override
+    public void start(Stage stage) {
+
+        NumberAxis xAxis = new NumberAxis(2006,2018,1);
+        NumberAxis yAxis = new NumberAxis(1,100,4);
+
+        // set the titles for the axes
+        xAxis.setLabel("Year");
+        yAxis.setLabel("Ranking");
+
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Iniversity of Helsinki, Shanghai ranking");
+
+        XYChart.Series data = new XYChart.Series();
+        data.setName("Helsinki");
+        data.getData().add(new XYChart.Data(2007,73));
+        data.getData().add(new XYChart.Data(2008,68));
+        data.getData().add(new XYChart.Data(2009,72));
+        data.getData().add(new XYChart.Data(2010,72));
+        data.getData().add(new XYChart.Data(2011,74));
+        data.getData().add(new XYChart.Data(2012,73));
+        data.getData().add(new XYChart.Data(2013,76));
+        data.getData().add(new XYChart.Data(2014,73));
+        data.getData().add(new XYChart.Data(2015,67));
+        data.getData().add(new XYChart.Data(2016,56));
+        data.getData().add(new XYChart.Data(2017,56));
+        
+        lineChart.getData().add(data);
+        
+        Scene scene = new Scene(lineChart);
+        stage.setScene(scene);
+        stage.show();
+        
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ShanghaiApplication.class);
     }
 
 }
--- part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
+++ part14-Part14_02.FinnishParties/src/main/java/application/PartiesApplication.java
@@ -1,9 +1,89 @@
 package application;
 
-public class PartiesApplication {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+public class PartiesApplication extends Application{
+
+    @Override
+    public void start(Stage window) throws Exception{
+        Map<String, Map<Integer,Integer>> values = getValues("partiesdata.tsv");
+        
+        NumberAxis xAxis = new NumberAxis(1968,2008,4);
+        NumberAxis yAxis = new NumberAxis(0,30,5);
+        
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("Iniversity of Helsinki, Shanghai ranking");
+
+        // go through the parties and add them to the chart
+        values.keySet().stream().forEach(party -> {
+            // a different data set for every party
+            XYChart.Series data = new XYChart.Series();
+            data.setName(party);
+
+            // add the party's support numbers to the data set
+            values.get(party).entrySet().stream().forEach(pair -> {
+                data.getData().add(new XYChart.Data(pair.getKey(), pair.getValue()));
+            });
+
+            // and add the data set to the chart
+            lineChart.getData().add(data);
+        });
+        
+        Scene scene = new Scene(lineChart,1000,1000);
+        window.setScene(scene);
+        window.show();
+    }
+    
+    
+    public Map<String, Map<Integer, Integer>> getValues(String input) throws Exception {
+        Map<String, Map<Integer, Integer>> res = new HashMap<>();
+        Scanner scanner = new Scanner(Paths.get(input));
+        
+        // Read the first line containing column headers (years)
+        String[] headers = scanner.nextLine().split("\t");
+        List<Integer> years = new ArrayList<>();
+
+        // Extract years from headers (ignoring the first column which is "Party")
+        for (int i = 1; i < headers.length; i++) {
+            years.add(Integer.parseInt(headers[i]));
+        }
+
+        // Read each subsequent line for party data
+        while (scanner.hasNextLine()) {
+            String[] line = scanner.nextLine().split("\t");
+            String party = line[0];
+            Map<Integer, Integer> yearData = new HashMap<>();
+
+            for (int i = 1; i < line.length; i++) {
+                String value = line[i];
+
+                // Convert numeric values to integer (rounding) and ignore "-"
+                if (!value.equals("-")) {
+                    yearData.put(years.get(i - 1), (int) Math.round(Double.parseDouble(value)));
+                }
+            }
+
+            res.put(party, yearData);
+        }
+
+        scanner.close();
+        return res;
+    }
+    
+    public static void main(String[] args) throws Exception{
+        launch(PartiesApplication.class);
     }
 
 }
